0819d2e7493ab80073db89c1f19f328d162a9b07
Oskar Svensson <osksv751@student.liu.se>
2021-03-31 14:07:22 +0200

    cleaned up copy.c

--- memcpy_testing/copy.c
+++ memcpy_testing/copy.c
@@ -7,16 +7,12 @@
 int copyMemory(int blockSize, int sourceSize, char* source, char* destination) {
   int numberOfCopies = sourceSize / blockSize + 1;
   for(int i = 0; i < numberOfCopies; i=i+1) {
-    //memcpy(destination + (i * blockSize), source + (i * blockSize), blockSize);
-
+    memcpy(destination + (i * blockSize), source + (i * blockSize), blockSize);
   }
-  //sleep(1);
   return numberOfCopies;
 }
 
 int main() {
-
-   //char src[10000] = "this is a pretty short string";
    char dest[2000000] = "something irrelevant";
    char src[2000000];
 
@@ -26,13 +22,13 @@ int main() {
       strncat(src, &c, 1);
     }
 
-   int loops = 1000;
+   int loops = 100;
    struct timespec ts1, ts2;
    long tsTot, tsAvr;
    for(int i = 0; i < loops; i=i+1) {
      clock_gettime(CLOCK_REALTIME, &ts1);
 
-     copyMemory(100,strlen(src), src, dest);
+     copyMemory(1000,strlen(src), src, dest);
 
      clock_gettime(CLOCK_REALTIME, &ts2);
      printf("%ld.%09ld\n", (long)(ts2.tv_sec - ts1.tv_sec),
@@ -42,10 +38,6 @@ int main() {
 
    tsAvr = tsTot / loops;
    printf("the averege time was: %ld\n", tsAvr);
-   //printf("%s\n", dest);
-   //printf("%ld.%09ld\n", (long)(ts2.tv_sec - ts1.tv_sec),
-    //ts2.tv_nsec - ts1.tv_nsec);
-
 
 
 }
